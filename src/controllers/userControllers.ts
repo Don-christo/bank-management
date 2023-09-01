ede661a75d9c381d1fdb559c3c9addc4817a3b1f
Ikechukwu Odokoro <odokoroikechukwujnr@gmail.com>
2023-09-01 16:15:11 +0100

    done

--- src/controllers/userControllers.ts
+++ src/controllers/userControllers.ts
@@ -1,9 +1,8 @@
-import express, { Request, Response, NextFunction } from 'express';
-import { generateAccountNumber } from '../utils/helpers'
-import path from 'path';
-import fs from 'fs';
-import { v4 } from 'uuid';
-
+import express, { Request, Response, NextFunction } from "express";
+import { generateAccountNumber } from "../utils/helpers";
+import path from "path";
+import fs from "fs";
+import { v4 } from "uuid";
 
 let bankFolder = path.join(__dirname, "../../src/bankDatabase");
 let bankFile = path.join(bankFolder, "bankDatabase.json");
@@ -14,7 +13,6 @@ export const createAccount = async (
   next: NextFunction
 ) => {
   try {
-
     // create database dynamically
     if (!fs.existsSync(bankFolder)) {
       fs.mkdirSync(bankFolder);
@@ -40,76 +38,84 @@ export const createAccount = async (
 
     const { accountName, dob, accountType, initialAccountBalance } = req.body;
 
-        if (!accountName || !accountType || !dob || initialAccountBalance === undefined) {
-            return res.status(400).json({
-                error: `Invalid input. Please provide valid credentials`
-            })
-        }
-    
-        const accountNumber = generateAccountNumber();
-    
-        const newAccount = {
-            accountNumber,
-            accountName,
-            dob,
-            accountType,
-            initialAccountBalance
-        };
-    
-        // add the account to a storage
-        allDetails.push(newAccount);
-    
-        // return unique account number with account details
-        const response = {
-            accountName,
-            accountNumber,
-            accountType,
-            initialAccountBalance
-        };
-    
-        res.status(200).json({
-            message: `Your account has been successfully created`,
-            response
-        });
+    if (
+      !accountName ||
+      !accountType ||
+      !dob ||
+      initialAccountBalance === undefined
+    ) {
+      return res.status(400).json({
+        error: `Invalid input. Please provide valid credentials`,
+      });
+    }
 
-    fs.writeFile(bankFile, JSON.stringify(allDetails, null, 2), "utf8", (err) => {
-      if (err) {
-        return res.status(500).json({
-          message: `Error creating this account: ${err.message}`,
-        });
-      } else {
-        return res.status(200).json({
-          message: `Bank details saved successfully`,
-          response,
-        });
-      }
+    const accountNumber = generateAccountNumber();
+
+    const newAccount = {
+      accountNumber,
+      accountName,
+      dob,
+      accountType,
+      initialAccountBalance,
+    };
+
+    // add the account to a storage
+    allDetails.push(newAccount);
+
+    // return unique account number with account details
+    const response = {
+      accountName,
+      accountNumber,
+      accountType,
+      initialAccountBalance,
+    };
+
+    res.status(200).json({
+      message: `Your account has been successfully created`,
+      response,
     });
+
+    fs.writeFile(
+      bankFile,
+      JSON.stringify(allDetails, null, 2),
+      "utf8",
+      (err) => {
+        if (err) {
+          return res.status(500).json({
+            message: `Error creating this account: ${err.message}`,
+          });
+        } else {
+          return res.status(200).json({
+            message: `Bank details saved successfully`,
+            response,
+          });
+        }
+      }
+    );
   } catch (err) {
     console.log(err);
   }
 };
 
-
 export const getAccount = (req: Request, res: Response) => {
-    const { accountNumber } = req.params;
-    console.log(accountNumber)
-    let bankAccounts: any[] = []
-    const data  =fs.readFileSync(bankFile, "utf-8")
+  const { accountNumber }  = req.params
 
-    bankAccounts = JSON.parse(data)
+  let bankAccounts: any[] = [];
+  const data = fs.readFileSync(bankFile, "utf-8");
 
-    // find the account from the storage
-    const account = bankAccounts.find((acc) => acc.accountNumber === accountNumber);
-    console.log(account)
+  bankAccounts = JSON.parse(data);
 
+  // find the account from the storage
+  const account = bankAccounts.find((acc) => acc.accountNumber === accountNumber);
 
-    if(!account) {
-        return res.status(404).json({
-            error: `Account not found`
-        })
-    }
-
-    return res.status(200).json({
-        message: `Your account is ${account}`
+  if (!account) {
+    return res.status(404).json({
+      error: `Account not found`,
     });
-}
+  }
+
+  return res.status(200).json({
+    message: `Your account is found successfully`,
+    data: account
+  });
+};
